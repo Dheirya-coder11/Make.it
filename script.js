97bb5ec0f9c056148803b9c61bd9f77a2d82c3db
Dheirya-coder11 <dheiryakharbanda101@gmail.com>
2024-07-05 10:41:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+gsap.from(".herosection h1", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".herosection p", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".about-herosection h1", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".about-herosection p", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".solution-herosection h1", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".solution-herosection p", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".journey-herosection h1", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".journey-herosection p", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".media-herosection h1", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
+
+gsap.from(".media-herosection p", {
+    y: 50,
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5
+})
